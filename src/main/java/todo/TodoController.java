a15d8e6a66217723520dda8ffb0cd0d9ac54d9f8
서영준 <seoyoungjun88@gmail.com>
2019-11-17 19:15:11 +0900

    Add todo list

---
+++ src/main/java/todo/TodoController.java
@@ -0,0 +1,17 @@
+package todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class TodoController {
+
+    @GetMapping("/todo")
+    public String todo(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "todo";
+    }
+
+}
